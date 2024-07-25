2a1ac5451cfdc81697495ae579b82c602e4fa11f
Maddie T <mtimm90136@uat.edu>
2024-07-24 18:04:27 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -1,25 +1,25 @@
 function rollTheDice() { 
 
-        var randomNumber1 = Math.floor(Math.random() * 6) + 1; 
-        var randomNumber2 = Math.floor(Math.random() * 6) + 1; 
+        var randomNumber1 = Math.floor(Math.random() * 6) + 1; //The math for my first dice, these create the contraints ofr the dice and the math
+        var randomNumber2 = Math.floor(Math.random() * 6) + 1; // The math for my second dice, this creates the constraints for my math and generates a random number 6 or below
 
-        document.querySelector(".img1").setAttribute("src", 
+        document.querySelector(".img1").setAttribute("src", //This is where we place the random number and string it together to call forth the picture
             "dice" + randomNumber1 + ".jpg"); 
 
-        document.querySelector(".img2").setAttribute("src", 
+        document.querySelector(".img2").setAttribute("src", //This is where we place the random number and string it together to call forth the picture
             "dice" + randomNumber2 + ".jpg"); 
 
-        if (((randomNumber1 + randomNumber2) == 7) || ((randomNumber1 + randomNumber2) == 11)) { 
-            document.querySelector("h1").innerHTML = "CRAPS - you lose!"; 
+        if (((randomNumber1 + randomNumber2) == 7) || ((randomNumber1 + randomNumber2) == 11)) { //This s my function showing conditionals and the actual function "guts"
+            document.querySelector("h1").innerHTML = "CRAPS - you lose!"; //I used the || operand to have the function fire if 7 OR 11 were chosen
         } 
 
-        else if (randomNumber1 = randomNumber2 && randomNumber1 % randomNumber2 == 0) { 
+        else if (randomNumber1 = randomNumber2 && randomNumber1 % randomNumber2 == 0) { //I have the function firing with the && to make sure both conditions are met
             document.querySelector("h1").innerHTML 
                             = ("You win!"); 
         } 
 
         else { 
-            document.querySelector("h1").innerHTML 
+            document.querySelector("h1").innerHTML //this covers all other cases
                             = ("You pushed"); 
         } 
 } 
